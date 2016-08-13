b5443292ec73cee0499aa435a0bca608294ee93b
Gabriel Rubens <grsabreu@gmail.com>
2016-08-12 21:28:10 -0300

    Import MongoInternals properly

--- packages/supercollection/get_collection.js
+++ packages/supercollection/get_collection.js
@@ -1,9 +1,9 @@
 import { Meteor } from 'meteor/meteor';
-import { Mongo } from 'meteor/mongo';
+import { MongoInternals } from 'meteor/mongo';
 
 export default function(name){
   if(Meteor.isServer)
-    return Mongo.MongoInternals.defaultRemoteCollectionDriver().open(name);
+    return MongoInternals.defaultRemoteCollectionDriver().open(name);
 
   return Meteor.connection._stores[name]._getCollection();
 }
